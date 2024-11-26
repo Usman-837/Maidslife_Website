730857b1cf3c87b923855e98940a32d47bdb7514
Usman <usmantufail837@gmail.com>
2024-11-26 18:05:46 +0500

    create feature component and add its basic structure and add heading, paragraph init

---
+++ src/Components/Features.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Features = () => {
+    return (
+        <div className='flex items-center justify-center'>
+            <div className='max-w-container py-12 px-8 text-center'>
+                <h2 className='text-3xl font-bold'>There are so many reasons to love Justlife!</h2>
+                <p className='text-lg mt-4'>Here are the top 4!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Features
