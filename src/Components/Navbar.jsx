6118896b3411aaf718f08df231b3ff4b34ad3f6c
Usman <usmantufail837@gmail.com>
2024-11-26 12:12:58 +0500

    add country logo and name

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from "../assets/logo.svg"
 import { HiMiniBars3 } from "react-icons/hi2"
 import { BiSolidUserCircle } from "react-icons/bi"
+import countrylogo from "../assets/sa.svg"
 
 const Navbar = () => {
     return (
@@ -10,9 +11,17 @@ const Navbar = () => {
                 <div>
                     <img src={logo} />
                 </div>
-                <div className='p-3 border border-gray-200 rounded-full flex gap-x-2 text-xl ml-3'>
-                    <BiSolidUserCircle />
-                    <HiMiniBars3 />
+                <div className='flex items-center'>
+                    <p className='p-3 text-xs font-semibold'>
+                        العربية     
+                    </p>
+                    <div className='p-3'>
+                        <img src={countrylogo} />
+                    </div>
+                    <div className='p-3 border border-gray-200 rounded-full flex gap-x-2 text-xl ml-3'>
+                        <BiSolidUserCircle />
+                        <HiMiniBars3 />
+                    </div>
                 </div>
             </div>
         </div>
