c432bf41d4771b617fe115a7f2c1f2a0f6dd57e4
Usman <usmantufail837@gmail.com>
2024-11-27 11:28:48 +0500

    create testimonial component, add its basic component and heading and paragraph init

---
+++ src/Components/Testimonial.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Testimonial = () => {
+    return (
+        <div>
+            <div className='flex items-center justify-center bg-[#f5f5f5]'>
+                <div className='max-w-container py-12 px-8 text-center'>
+                    <h2 className='text-3xl font-bold'>What customers say about Justlife</h2>
+                    <p className='text-lg mt-4'>Justlife has been rated 4.8 out of 5 based on 1626 reviews as of March 2024.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Testimonial
