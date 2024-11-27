b748f6f3fddfa02d9c92a2c507dfdbc46b82d489
Usman <usmantufail837@gmail.com>
2024-11-27 17:54:55 +0500

    add more countries and cities in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,14 +2,33 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='flex items-center justify-center bg-[#212121]'>
+        <div className='flex items-center justify-center bg-[#212121] py-2 px-4'>
             <div className='max-w-container w-full text-white'>
-                <div>
-                    <p>United Arab Emirates</p>
+                <div className='flex justify-between'>
+                    <div className='flex flex-col items-center justify-center'>
+                        <p className='py-4 text-lg font-semibold'>United Arab Emirates</p>
+                        <div className='pb-6 flex flex-col text-center w-36 gap-2'>
+                            <a href='' className='bg-black rounded-full px-2 py-0.5 text-sm'>Dubai</a>
+                            <a href='' className='bg-black rounded-full px-2 py-0.5 text-sm'>Abu Dhabi</a>
+                            <a href='' className='bg-black rounded-full px-2 py-0.5 text-sm'>Sharjah</a>
+                            <a href='' className='bg-black rounded-full px-2 py-0.5 text-sm'>Ajman</a>
+                        </div>
+                    </div>
                     <div>
-                        <a href='' className='bg-black rounded-full px-2 py-0.5 text-sm'>Dubai</a>
+                        <p className='py-4 text-lg text-center font-semibold'>Saudi Arabia</p>
+                        <div className='pb-6 flex flex-col text-center w-36 gap-2'>
+                            <a href='' className='bg-black rounded-full px-2 py-0.5 text-sm'>Jeddah</a>
+                            <a href='' className='bg-black rounded-full px-2 py-0.5 text-sm'>Riyadh</a>
+                        </div>
+                    </div>
+                    <div>
+                        <p className='py-4 text-lg text-center font-semibold'>Qatar</p>
+                        <div className='pb-6 flex flex-col text-center w-36 gap-2'>
+                            <a href='' className='bg-black rounded-full px-2 py-0.5 text-sm'>Doha</a>
+                        </div>
                     </div>
                 </div>
+
             </div>
         </div>
     )
