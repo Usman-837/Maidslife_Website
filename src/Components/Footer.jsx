a78016b5f5f246602fd315cee4be2ee6f6470bfc
Usman <usmantufail837@gmail.com>
2024-11-27 18:30:29 +0500

    add responsive funcyionality to make footer responsive

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
     return (
         <div className='flex items-center justify-center bg-[#212121] py-2 px-4'>
             <div className='max-w-container w-full text-white'>
-                <div className='flex justify-between'>
+                <div className='flex justify-between max-md:grid max-md:grid-cols-2 max-sm:grid-cols-1'>
                     <div className='flex flex-col items-center justify-center'>
                         <p className='py-4 text-lg font-semibold'>United Arab Emirates</p>
                         <div className='pb-6 flex flex-col text-center w-36 gap-2'>
@@ -20,21 +20,21 @@ const Footer = () => {
                             <a href='' className='bg-black rounded-full px-2 py-0.5 text-sm'>Ajman</a>
                         </div>
                     </div>
-                    <div>
+                    <div className='flex flex-col items-center justify-center'>
                         <p className='py-4 text-lg text-center font-semibold'>Saudi Arabia</p>
                         <div className='pb-6 flex flex-col text-center w-36 gap-2'>
                             <a href='' className='bg-black rounded-full px-2 py-0.5 text-sm'>Jeddah</a>
                             <a href='' className='bg-black rounded-full px-2 py-0.5 text-sm'>Riyadh</a>
                         </div>
                     </div>
-                    <div>
+                    <div className='flex flex-col items-center justify-center'>
                         <p className='py-4 text-lg text-center font-semibold'>Qatar</p>
                         <div className='pb-6 flex flex-col text-center w-36 gap-2'>
                             <a href='' className='bg-black rounded-full px-2 py-0.5 text-sm'>Doha</a>
                         </div>
                     </div>
                 </div>
-                <div className='border-t border-gray-700 mt-6 pt-4 pb-2 flex items-center justify-between'>
+                <div className='border-t border-gray-700 mt-6 pt-4 pb-2 flex items-center justify-between max-md:grid max-md:grid-cols-2 max-md:gap-y-10 max-sm:grid-cols-1'>
                     <div className='flex gap-x-4'>
                         <img src={appstore} className='w-32 h-10' />
                         <img src={playstore} className='w-32 h-10' />
