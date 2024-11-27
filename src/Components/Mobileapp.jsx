903bb642cac361250143a82c86cf858435910c5b
Usman <usmantufail837@gmail.com>
2024-11-27 16:52:54 +0500

    add mobileapp image

--- src/Components/Mobileapp.jsx
+++ src/Components/Mobileapp.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import appstore from "../assets/appstore.png"
 import playstore from "../assets/playstore.png"
+import appimg from "../assets/apps.webp"
 
 const Mobileapp = () => {
   return (
     <div className='flex items-center justify-center'>
-      <div className='max-w-container pt-10'>
+      <div className='max-w-container w-full pt-10 flex justify-between'>
         <div className='max-w-[570px] w-full'>
           <span className='text-[#00000061] mb-2'>Download our Super app!</span>
           <p className='text-3xl font-semibold'>Manage all to-dos with a single tap!</p>
           <p className='text-lg mt-4'>Book and manage your appointments, view your professional’s profile and ratings, get the latest offers, and much more.</p>
           <div className='flex my-8 gap-x-9'>
-            <img src={appstore} className='w-32 h-10'/>
-            <img src={playstore} className='w-32 h-10'/>
+            <img src={appstore} className='w-32 h-10' />
+            <img src={playstore} className='w-32 h-10' />
           </div>
+        </div>
+        <div>
+          <img src={appimg} className='w-96' />
         </div>
       </div>
     </div>
