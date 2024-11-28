c78a9f600acde0fba73711ed30121eb30f1b12e7
Usman <usmantufail837@gmail.com>
2024-11-28 13:44:01 +0500

    add header section in homecleaning compenent

--- src/Components/Services/Homecleaning/Homecleaning.jsx
+++ src/Components/Services/Homecleaning/Homecleaning.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import Navbar from '../../Navbar'
 import Footer from '../../Footer'
+import { FaArrowLeft } from "react-icons/fa"
 
 
 const Homecleaning = () => {
   return (
-    <div>
-      <Navbar/>
-      <Footer/>
-    </div>
+    <>
+      <Navbar />
+      <div className='flex items-center justify-center'>
+        <section className='max-w-container w-full'>
+          <header className='py-5'>
+            <div className='flex items-center gap-x-3 pb-2'>
+              <FaArrowLeft className='text-xl'/>
+              <span className='font-semibold'>Step 1 of 4</span>
+            </div>
+            <div>
+              <h2 className='font-bold text-xl'>Service Details</h2>
+            </div>
+          </header>
+        </section>
+      </div>
+      <Footer />
+    </>
   )
 }
 
