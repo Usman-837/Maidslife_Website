bae979eebe4e15e05d4bd19a0ae13e6da98d1421
Usman <usmantufail837@gmail.com>
2024-11-28 15:18:05 +0500

    add hours heading and info icon in homecleaning component

--- src/Components/Services/Homecleaning/Homecleaning.jsx
+++ src/Components/Services/Homecleaning/Homecleaning.jsx
@@ -2,23 +2,32 @@ import React from 'react'
 import Navbar from '../../Navbar'
 import Footer from '../../Footer'
 import { FaArrowLeft } from "react-icons/fa"
+import { BsInfoCircleFill } from "react-icons/bs"
 
 
 const Homecleaning = () => {
   return (
     <>
       <Navbar />
-      <div className='flex items-center justify-center'>
+      <div className='flex items-center justify-center bg-[#fafafa]'>
         <section className='max-w-container w-full'>
           <header className='py-5'>
             <div className='flex items-center gap-x-3 pb-2'>
-              <FaArrowLeft className='text-xl'/>
+              <FaArrowLeft className='text-xl' />
               <span className='font-semibold'>Step 1 of 4</span>
             </div>
             <div>
               <h2 className='font-bold text-xl'>Service Details</h2>
             </div>
           </header>
+          <main className='pb-16'>
+            <div className='bg-white max-w-[558px] w-full h-80 border rounded-xl p-6'>
+              <div className='flex items-center gap-x-2 pb-4'>
+                <h4 className='font-semibold text-lg'>How many hours do you need your professional to stay?</h4>
+                <BsInfoCircleFill className='text-gray-600'/>
+              </div>
+            </div>
+          </main>
         </section>
       </div>
       <Footer />
