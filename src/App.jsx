a8c63941a3bf7e00d4aa26472033086ad6b74774
Usman <usmantufail837@gmail.com>
2024-11-28 11:13:40 +0500

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import Banner from './Components/Banner'
 import Services from './Components/Services'
 import Features from './Components/Features'
 import Testimonial from './Components/Testimonial'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-      <Banner/>
-      <Services/>
-      <Features/>
-      <Testimonial/>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route to="/header" element={<Navbar />} />
+          <Route to="/banner" element={<Banner />} />
+          <Route to="/services" element={<Services />} />
+          <Route to="/features" element={<Features />} />
+          <Route to="/testimonial" element={<Testimonial />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
