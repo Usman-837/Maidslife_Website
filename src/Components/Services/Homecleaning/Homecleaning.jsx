81c9c3530f6b9cc1432a7f9c078ba49f30141332
Usman <usmantufail837@gmail.com>
2024-11-28 17:07:45 +0500

    create seperate component for homecleaning services details and rearrange the code in homecleaning component

--- src/Components/Services/Homecleaning/Homecleaning.jsx
+++ src/Components/Services/Homecleaning/Homecleaning.jsx
@@ -1,76 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Navbar from '../../Navbar'
 import Footer from '../../Footer'
 import { FaArrowLeft } from "react-icons/fa"
-import { BsInfoCircleFill } from "react-icons/bs"
+import Servicesdetails from './Servicesdetails'
 
 
 const Homecleaning = () => {
-
-  const [text, setText] = useState('');
-  const charLimit = 150; // Maximum character limit
-
-  const handleChange = (e) => {
-    const inputText = e.target.value;
-
-    if (inputText.length <= charLimit) {
-      setText(inputText); // Allow input only within character limit
-    }
-  };
-
-  const Hours = [
-    {
-      id: 1,
-      hours: "1"
-    },
-    {
-      id: 2,
-      hours: "2"
-    },
-    {
-      id: 3,
-      hours: "3"
-    },
-    {
-      id: 4,
-      hours: "4"
-    },
-    {
-      id: 5,
-      hours: "5"
-    },
-    {
-      id: 6,
-      hours: "6"
-    },
-    {
-      id: 7,
-      hours: "7"
-    },
-    {
-      id: 8,
-      hours: "8"
-    }
-  ]
-
-  const Professionals = [
-    {
-      id: 1,
-      prof: "1"
-    },
-    {
-      id: 2,
-      prof: "2"
-    },
-    {
-      id: 3,
-      prof: "3"
-    },
-    {
-      id: 4,
-      prof: "4"
-    }
-  ]
 
   return (
     <>
@@ -82,63 +17,13 @@ const Homecleaning = () => {
               <FaArrowLeft className='text-xl' />
               <span className='font-semibold'>Step 1 of 4</span>
             </div>
-            <div>
-              <h2 className='font-bold text-xl'>Service Details</h2>
+            <div className='mt-7'>
+              <h2 className='font-semibold text-3xl'>Service Details</h2>
             </div>
           </header>
           <main className='pb-16'>
             <div className='bg-white max-w-[558px] w-full border rounded-xl p-6'>
-              {/* Hours Selection */}
-              <div className='flex items-center gap-x-2 pb-4'>
-                <h4 className='font-semibold text-lg'>How many hours do you need your professional to stay?</h4>
-                <BsInfoCircleFill className='text-gray-600' />
-              </div>
-              <div className='flex gap-x-2 pb-9'>
-                {
-                  Hours.map((data) => {
-                    return (
-                      <div className='w-10 h-10 border border-gray-500 hover:border-[#00c3ff] rounded-full flex items-center justify-center font-semibold hover:text-[#00c3ff] active:bg-[#d9f6ff]'>{data.hours}</div>
-                    )
-                  })
-                }
-              </div>
-              {/* Professional Selection */}
-              <div className='pb-4'>
-                <h4 className='font-semibold text-lg'>How many professionals do you need?</h4>
-              </div>
-              <div className='flex gap-x-2 pb-9'>
-                {
-                  Professionals.map((data) => {
-                    return (
-                      <div className='w-10 h-10 border border-gray-500 hover:border-[#00c3ff] rounded-full flex items-center justify-center font-semibold hover:text-[#00c3ff] active:bg-[#d9f6ff]'>{data.prof}</div>
-                    )
-                  })
-                }
-              </div>
-              {/* Cleaning Material Buttons */}
-              <div className='flex items-center gap-x-2 pb-4'>
-                <h4 className='font-semibold text-lg'>Need cleaning materials?</h4>
-                <BsInfoCircleFill className='text-gray-600' />
-              </div>
-              <div className='flex gap-x-2 pb-9'>
-                <div className='px-4 py-2 border border-gray-500 hover:border-[#00c3ff] rounded-full flex items-center justify-center font-semibold hover:text-[#00c3ff] active:bg-[#d9f6ff]'>No, I have them</div>
-                <div className='px-4 py-2 border border-gray-500 hover:border-[#00c3ff] rounded-full flex items-center justify-center font-semibold hover:text-[#00c3ff] active:bg-[#d9f6ff]'>Yes, Please</div>
-              </div>
-              {/* Instructions */}
-              <div className='pb-4'>
-                <h4 className='font-semibold text-lg'>Any instructions or special requirements?</h4>
-              </div>
-              <div className="relative w-full max-w-lg">
-                <textarea
-                  value={text}
-                  onChange={handleChange}
-                  placeholder="Example: Key under the mat, ironing, window cleaning, etc."
-                  className="w-full h-32 p-4 border border-gray-300 rounded-lg focus:outline-none focus:border-[#00c3ff] resize-none"
-                />
-                <div className="absolute bottom-2 right-4 text-gray-500 text-sm">
-                  {text.length}/{charLimit}
-                </div>
-              </div>
+              <Servicesdetails />
               {/* Next Button */}
               <div className='py-6'>
                 <p className='text-center bg-[#FFD03E] hover:bg-yellow-400 py-3 rounded-full text-white font-bold'>Next</p>
