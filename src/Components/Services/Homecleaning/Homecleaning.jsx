32ccc1c76ed93a266e7057725142bd031c9141a7
Usman <usmantufail837@gmail.com>
2024-11-28 15:39:56 +0500

    add hours select circle buttons

--- src/Components/Services/Homecleaning/Homecleaning.jsx
+++ src/Components/Services/Homecleaning/Homecleaning.jsx
@@ -6,6 +6,43 @@ import { BsInfoCircleFill } from "react-icons/bs"
 
 
 const Homecleaning = () => {
+
+  const Hours = [
+    {
+      id: 1,
+      hours: "1"
+    },
+    {
+      id: 2,
+      hours: "2"
+    },
+    {
+      id: 3,
+      hours: "3"
+    },
+    {
+      id: 4,
+      hours: "4"
+    },
+    {
+      id: 5,
+      hours: "5"
+    },
+    {
+      id: 6,
+      hours: "6"
+    },
+    {
+      id: 7,
+      hours: "7"
+    },
+    {
+      id: 8,
+      hours: "8"
+    }
+  ]
+
+
   return (
     <>
       <Navbar />
@@ -24,7 +61,16 @@ const Homecleaning = () => {
             <div className='bg-white max-w-[558px] w-full h-80 border rounded-xl p-6'>
               <div className='flex items-center gap-x-2 pb-4'>
                 <h4 className='font-semibold text-lg'>How many hours do you need your professional to stay?</h4>
-                <BsInfoCircleFill className='text-gray-600'/>
+                <BsInfoCircleFill className='text-gray-600' />
+              </div>
+              <div className='flex gap-x-2 pb-9'>
+                {
+                  Hours.map((data) => {
+                    return (
+                      <div className='w-10 h-10 border border-gray-500 hover:border-[#00c3ff] rounded-full flex items-center justify-center font-semibold hover:text-[#00c3ff] active:bg-[#d9f6ff]'>{data.hours}</div>
+                    )
+                  })
+                }
               </div>
             </div>
           </main>
