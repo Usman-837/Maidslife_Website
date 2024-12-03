b2f893ed62bc7b58c8c547410952fdb5accb5613
Usman <usmantufail837@gmail.com>
2024-12-03 11:03:48 +0500

    add frequency heading box

--- src/Components/Services/Homecleaning/DateTime.jsx
+++ src/Components/Services/Homecleaning/DateTime.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { LuRefreshCw } from "react-icons/lu"
 
 const DateTime = () => {
   return (
-    <div>
-      <h1>Date and Time</h1>
-    </div>
+    <>
+      <div>
+        <div>
+          {/* Frequency Box */}
+          <div className='flex flex-col gap-2 border border-[#00C3FF] px-5 py-3 rounded-lg bg-[#ebfaff]'>
+            <div className='flex justify-between'>
+              <h4 className='text-lg font-bold'>Frequency</h4>
+              <a href='' className='text-[#00C3FF] underline font-semibold'>Change</a>
+            </div>
+            <div className='bg-[#00C3FF] text-white flex items-center font-semibold gap-x-2 p-2 rounded-lg'>
+              <LuRefreshCw />
+              <p>One Time Service</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
