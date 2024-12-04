d05d1dce5bc00b8ae221a7b9ecb798820e21863a
Usman <usmantufail837@gmail.com>
2024-12-04 14:54:56 +0500

    add responsive functionality in testimonial componnets to make it responsive

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -27,6 +27,13 @@ const Testimonial = () => {
             {
                 breakpoint: 1280,
                 settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 993,
+                settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
                 },
@@ -61,19 +68,19 @@ const Testimonial = () => {
 
     return (
         <div className="flex items-center justify-center bg-[#f5f5f5] py-12 px-8">
-            <div className="max-w-container text-center">
-                <h2 className="text-3xl font-bold">What customers say about Justlife</h2>
-                <p className="text-lg mt-4">
+            <div className="max-w-container w-full text-center">
+                <h2 className="text-3xl max-sm:text-2xl font-bold">What customers say about Justlife</h2>
+                <p className="text-lg max-sm:text-sm mt-4">
                     Justlife has been rated 4.8 out of 5 based on 1626 reviews as of March 2024.
                 </p>
                 <div className="py-12">
                     <Slider {...settings}>
                         {testimonialcards.map((data) => (
-                            <div key={data.id} className='px-3 max-xl:px-0 flex'>
-                                <div className="relative mt-4 py-9 px-6 h-96 flex flex-col items-center justify-center bg-white rounded-lg shadow-md">
-                                    <p className="text-[#4A4A4A] text-lg my-6 text-justify">{data.des}</p>
-                                    <p className="text-3xl font-bold text-[#00c3ff]">{data.title}</p>
-                                    <div className="absolute flex text-3xl top-[-20px] z-50 py-1.5 px-2.5 bg-white border shadow-lg text-yellow-400 rounded-full">
+                            <div key={data.id} className='px-3'>
+                                <div className="relative mt-4 py-9 px-6 h-96 max-sm:h-72 max-mobile:h-96 flex flex-col items-center justify-center bg-white rounded-lg shadow-md">
+                                    <p className="text-[#4A4A4A] text-lg max-sm:text-sm my-6 text-justify">{data.des}</p>
+                                    <p className="text-3xl max-sm:text-2xl font-bold text-[#00c3ff]">{data.title}</p>
+                                    <div className="absolute flex text-3xl max-sm:text-2xl top-[-20px] z-50 py-1.5 px-2.5 bg-white border shadow-lg text-yellow-400 rounded-full">
                                         <LiaStarSolid />
                                         <LiaStarSolid />
                                         <LiaStarSolid />
